5efcf7015bded0b97bee937cbebf94c905104dd0
Gustavo José Lemos Mendes <gustavo.mendes@w-security.intranet>
2023-09-27 16:58:30 -0300

    Adicioandno loading enquanto os nossos dados não chegam da API

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,17 @@ import { useCurrentLesson } from "../store/slices/player";
 
 export function Header() {
   const { currentModule, currentLesson } = useCurrentLesson();
+  const isCourseLoading = useAppSelector(state => state.player.isLoading);
+
+  if (isCourseLoading) {
+    <h1 className="text-2xl font-bold">Carregando...</h1>
+  }
+
 
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">{currentModule.title}</h1>
-      <span className="text-sm text-zinc-400">Módulo "{currentLesson.title}"</span>
+      <h1 className="text-2xl font-bold">{currentModule?.title}</h1>
+      <span className="text-sm text-zinc-400">Módulo "{currentLesson?.title}"</span>
     </div>
   );
 }
